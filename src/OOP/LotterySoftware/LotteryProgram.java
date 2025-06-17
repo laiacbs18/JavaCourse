27430a8ec35abd7da3b6c3826338a33fa05f9e09
Laia Bordas <laiacbs18@gmail.com>
2025-06-17 12:20:13 -0400

    Created main class

---
+++ src/OOP/LotterySoftware/LotteryProgram.java
@@ -0,0 +1,12 @@
+public class LotteryProgram {
+    public static void main(String[] args) {
+        // Lottery Program
+        /* 
+            * The program should randomly generate a winning lottery ticket.
+            * Prompt the player to enter lotto numbers from 1 to 400.
+            * Depending on how many numbers match, the player wins various prizes.
+        */
+
+        
+    }
+}
